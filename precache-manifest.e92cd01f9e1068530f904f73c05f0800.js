d31d0bcc83ce402604ed7939a6e7bffea4d7caa0
satya-srikar <nsatyasrikar@gmail.com>
2020-07-05 09:12:21 +0530

    Updates

---
+++ precache-manifest.e92cd01f9e1068530f904f73c05f0800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bdb893af59d778e6dca2c254385b3b",
+    "url": "/employes-list/index.html"
+  },
+  {
+    "revision": "379197787f707b3e73d3",
+    "url": "/employes-list/static/css/main.92a1069c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/employes-list/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employes-list/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379197787f707b3e73d3",
+    "url": "/employes-list/static/js/main.8c4c8d25.chunk.js"
+  },
+  {
+    "revision": "2c5ff3deed9a5782a5d8",
+    "url": "/employes-list/static/js/runtime-main.a17c25e3.js"
+  }
+]);
